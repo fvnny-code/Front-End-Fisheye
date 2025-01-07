bde663b3e847abb5912b684bfd5cf3b5448c515f
fvnny-code <f.codecco.grando@gmail.com>
2025-01-07 10:44:14 +0100

    mise à jour du chemin absolu de récuprération des données

--- scripts/utils/data.js
+++ scripts/utils/data.js
@@ -1,26 +1,28 @@
-
-import Api from './Api.js'; 
+import Api from "./Api.js";
 
 export async function getPhotographers() {
-    const api = new Api('../data/photographers.json');  // Spécifier l'URL du fichier JSON
-    const data = await api.get();  
-    return data;
+  const baseUrl =
+    window.location.hostname === "localhost"
+      ? "../data/photographers.json"
+      : "https://fvnny-code.github.io/Front-End-Fisheye/data/photographers.json";
+  const api = new Api(baseUrl);
+  const data = await api.get();
+  return data;
 }
 
 export async function getMedias() {
-    try {
-        const fetchMedias = await fetch("data/photographers.json");
-        if (!fetchMedias.ok) {
-            throw new Error('Erreur récup data');
-        }
-        const dataMedias = await fetchMedias.json();
-        const medias = dataMedias.media;
-        return { medias };
-    } catch (error) {
-        console.error("Erreur: ", error);
-        return {
-            medias: []
-        };
+  try {
+    const fetchMedias = await fetch("data/photographers.json");
+    if (!fetchMedias.ok) {
+      throw new Error("Erreur récup data");
     }
+    const dataMedias = await fetchMedias.json();
+    const medias = dataMedias.media;
+    return { medias };
+  } catch (error) {
+    console.error("Erreur: ", error);
+    return {
+      medias: [],
+    };
+  }
 }
-
